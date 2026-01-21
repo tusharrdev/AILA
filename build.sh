a2404eec9ae38c2d3be234c62359f924ba4377a6
tushar-4 <tusharhegde27@gmail.com>
2026-01-21 08:51:32 +0530

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-pip install --upgrade pip
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
+# Don't use pip from venv, use system pip
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --no-input
+python3 manage.py migratecls
