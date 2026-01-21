369c7fa49bd3237d756cfa8dac11d75b3af0b46d
tushar-4 <tusharhegde27@gmail.com>
2026-01-21 09:06:21 +0530

    Use venv pip for installation

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-# Remove any existing venv
-rm -rf .venv
-# Install globally
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --no-input
-python3 manage.py migrate
+pip install --upgrade pip
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
