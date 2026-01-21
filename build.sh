b2aa4a5a285e7d221c48a2f31e0495c26ae4af58
tushar-4 <tusharhegde27@gmail.com>
2026-01-21 09:00:50 +0530

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Remove any existing venv
+rm -rf .venv
+# Install globally
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --no-input
